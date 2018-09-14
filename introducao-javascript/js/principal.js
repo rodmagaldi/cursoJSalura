24506b160b1071bd8bc961220734fc6c8693c905
rodmagaldi <rod.magaldi@gmail.com>
2018-09-14 12:22:55 -0300

    add my stuff

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,47 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i]
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoValido = true;
+    if (peso < 0 || peso >= 1000) {
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido!"
+        paciente.classList.add("paciente-invalido")
+    }
+
+    var alturaValida = true;
+    if (altura < 0 || altura > 3) {
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida!"
+        paciente.classList.add("paciente-invalido")
+    }
+
+    if (pesoValido && alturaValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+}
+
+titulo.addEventListener("click", function() { console.log("Você clicou no título!") });
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Oi, cliquei no botão!");
+
+});
