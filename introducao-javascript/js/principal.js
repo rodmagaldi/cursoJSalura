9d6db5b851ef4c35de95945b2ae0899cacecb31b
rodmagaldi <rod.magaldi@gmail.com>
2018-09-14 16:10:58 -0300

    Add the elements to the table by clicking on the button

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -37,10 +37,41 @@ for (i = 0; i < pacientes.length; i++) {
 
 }
 
-titulo.addEventListener("click", function() { console.log("Você clicou no título!") });
+titulo.addEventListener("click", function() {
+    console.log("Você clicou no título!");
+});
 
 var botaoAdd = document.querySelector("#adicionar-paciente");
 botaoAdd.addEventListener("click", function(event) {
     event.preventDefault();
     console.log("Oi, cliquei no botão!");
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
 });
