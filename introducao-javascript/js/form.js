098560f9d499936c6a2bb22af1e8c07eaec882b5
rodmagaldi <rod.magaldi@gmail.com>
2018-09-16 15:30:46 -0300

    Further separation

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -34,28 +34,20 @@ function montaTr(paciente) {
     var pacienteTr = document.createElement("tr");
     pacienteTr.classList.add("paciente");
 
-    var nomeTd = document.createElement("td");
-    nomeTd.classList.add("info-nome");
-    var pesoTd = document.createElement("td");
-    pesoTd.classList.add("info-peso");
-    var alturaTd = document.createElement("td");
-    alturaTd.classList.add("info-altura");
-    var gorduraTd = document.createElement("td");
-    gorduraTd.classList.add("info-gordura");
-    var imcTd = document.createElement("td");
-    imcTd.classList.add("info-imc");
-
-    nomeTd.textContent = paciente.nome;
-    pesoTd.textContent = paciente.peso;
-    alturaTd.textContent = paciente.altura;
-    gorduraTd.textContent = paciente.gordura;
-    imcTd.textContent = paciente.imc;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
     return pacienteTr;
+}
+
+function montaTd(dado, classe) {
+
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+
+    return td;
 }
